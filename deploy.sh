7f90b026baef82ca1f0e3cf38fa8ea392dda33c4
ChenGang <chengang@zzltop.com>
2023-12-01 13:57:17 +0800

    修改部署文件

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@gitee.com:pakhoc/Aincrad.git master:gh-pages
+git push -f git@github.com:Pakho12138/Aincrad.git master:gh-pages
 
 cd -
